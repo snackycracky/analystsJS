c398a7495424605201296a704902f36795c90d09
nils petersohn <post@berlin.de>
2012-02-11 21:42:18 +0100

    format

--- src/driver.js
+++ src/driver.js
@@ -6,23 +6,29 @@ var http = require("http"),
 var rssItems_emitter = new events.EventEmitter();
 
 function get_feeds() {
+
     console.log("making request now");
-    var req = rss_client.request("GET", "/rss/analysen.rss2" );
+    var req = rss_client.request("GET", "/rss/analysen.rss2");
     req.end();
+
     console.log("request sent");
-    req.on('response', function(response){
+    req.on('response', function (response) {
+
         console.log("response recieved");
+        response.setEncoding("utf8");
+
+
         var body = "";
-        response.addListener("data", function(data) {
+        response.addListener("data", function (data) {
             body += data;
         });
 
-        response.addListener("end", function() {
+        response.addListener("end", function () {
             console.log("parsing response now")
             var parser = new xml2js.Parser();
-            parser.addListener('end', function(result) {
+            parser.addListener('end', function (result) {
 
-                result.channel.item.forEach(function(item){
+                result.channel.item.forEach(function (item) {
                     console.log(item.title)
                 })
 
