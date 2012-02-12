4a1b5479e2e907e02a333c3e20535aad70942a1a
nils petersohn <post@berlin.de>
2012-02-12 13:32:09 +0100

    not working due node.js error

--- src/analyseParser.js
+++ src/analyseParser.js
@@ -1,36 +1,38 @@
 var http = require("http"),
         rss_client = http.createClient(80, "aktiencheck.de"),
-        xml2js = require('xml2js');
+        xml2js = require('xml2js'),
+        mongodb = require('mongodb');
 
-var db = require('riak-js').getClient({host:"127.0.0.1", port:"8098"  })
+var server = new mongodb.Server("127.0.0.1", 27017, {});
+var db = new mongodb.Db('analystsOpinions', server, {native_parser:true})
 var x = 0;
-function get_analyse(url) {
+function get_analyse( url) {
 
+    try {
+        var req = null;
+        var ident = url.match(new RegExp(/-([0-9])*/g))[1].replace("-", "")
 
-    var req = null;
-    var ident = url.match(new RegExp(/-([0-9])*/g))[1].replace("-", "")
+        req = http.createClient(80, "aktiencheck.de").request("GET", url);
 
-    req = http.createClient(80, "aktiencheck.de").request("GET", url);
+        req.end();
 
-    req.end();
+        //console.log("request sent");
+        req.on('response', function (response) {
 
-    //console.log("request sent");
-    req.on('response', function (response) {
+            //console.log("response recieved");
 
-        //console.log("response recieved");
+            var body = "";
+            response.addListener("data", function (data) {
 
-        var body = "";
-        response.addListener("data", function (data) {
+                body += data;
+            });
 
-            body += data;
-        });
+            response.addListener("end", function (data) {
 
-        response.addListener("end", function (data) {
+                //http://stackoverflow.com/questions/1068280/javascript-regex-multiline-flag-doesnt-work
 
-            //http://stackoverflow.com/questions/1068280/javascript-regex-multiline-flag-doesnt-work
+                var arrMatches = body.match(new RegExp(/h2([\s\S]*)content_right/i));
 
-            var arrMatches = body.match(new RegExp(/h2([\s\S]*)content_right/i));
-            try {
                 var time = arrMatches[0].match(new RegExp(/([0-2]{0,1}[0-9]|30|31)\.([01]{0,1}[0-9])\.([0-9]{2})\s([0-9]{2}):([0-9]{2})/g))[0]
                 if (arrMatches == null) {
                     console.log("err");
@@ -44,29 +46,36 @@ function get_analyse(url) {
                 var title = arrMatches[0].match(new RegExp(/h2>(.*)<\/h2/g))[0]
 
                 var ident = url.match(new RegExp(/-([0-9])*/g))[1].replace("-", "")
-                db.get('analystsOpinions', ident, function (err, entry, meta) {
-                    if (entry == undefined) {
-                        console.log("entering new")
 
-                        db.save("analystsOpinions", ident, {
-                            title:title,
-                            ident:ident,
-                            link:url,
-                            time:time,
-                            content:analysenContent
-                        });
+                db.open(function (error, client) {
+                    if (client != null) {
+                        var collection = new mongodb.Collection(client, 'analystsOpinions');
+                        try {
+                            var insert = function () {
+                                collection.insert(
+                                        {id:ident, title:title, link:url, time:new Date(time), content:analysenContent});
+                                console.log("insert: " + ident)
+                            }
+                            insert();
+                            collection.find({id:ident}, {limit:10}).toArray(function (err, foundDocs) {
+                                if (foundDocs == null || foundDocs.length == 0) {
+                                    console.log("inserting: " + ident + " was not successful")
+                                    insert();
+                                }
+                            });
 
-
-                        db.count('analystsOpinions')
-                    } else{
-                        console.log("exsists");
+                        } catch (x) {
+                            console.log("ERROR");
+                        }
                     }
                 });
-            } catch (x) {
-                console.log("ERROR");
-            }
-        });
 
-    });
+
+            });
+
+        });
+    } catch (x) {
+        console.log("ERROR");
+    }
 }
 exports.get_analyse = get_analyse;
--- src/driver.js
+++ src/driver.js
@@ -1,10 +1,13 @@
 var http = require("http"),
         rss_client = http.createClient(80, "aktiencheck.de"),
         xml2js = require('xml2js'),
-        analyseParser = require("./analyseParser.js");
+        analyseParser = require("./analyseParser.js"),
+        mongodb = require('mongodb');
 
+
+var server = new mongodb.Server("127.0.0.1", 27017, {});
+var db = new mongodb.Db('analystsOpinions', server, {native_parser:true})
 var bucketName = "anlystOpinions";
-var db = require('riak-js').getClient({host:"127.0.0.1", port:"8098"})
 
 function get_feeds(pageNr) {
 
@@ -29,20 +32,28 @@ function get_feeds(pageNr) {
 
             arrMatches.forEach(function (link) {
                 async_function(true, function (val) {
-                    var ident = link.match(new RegExp(/-([0-9])*/g))[1].replace("-", "")
-
-                    db.get('analystsOpinions', ident, function (err, entry, meta) {
-                        if (entry == undefined) {
-                            analyseParser.get_analyse(link.replace("\"", ""));
-                        }
-                    });
+//                    var ident = link.match(new RegExp(/-([0-9])*/g))[1].replace("-", "")
+//                    db.open(function (error, client) {
+//                        if (error) {
+//                            throw error
+//                        }
+//                        if (client != null) {
+//                            var collection = new mongodb.Collection(client, 'analystsOpinions');
+//
+//                            collection.find({id:ident}, {limit:10}).toArray(function (err, foundDocs) {
+//                                if (foundDocs.length == 0) {
+                                    analyseParser.get_analyse(link.replace("\"", ""));
+//                                }
+//                            });
+//                        }
+//                    });
                 });
             });
         });
 
     });
 }
-//
+
 var async_function = function (val, callback) {
     process.nextTick(function () {
         callback(val);
@@ -50,9 +61,23 @@ var async_function = function (val, callback) {
 };
 
 
-for (var i = 1; i < 3629; i++) {
+for (var i = 3000; i < 3629; i++) {//3629
     get_feeds(i)
-    console.log("going to next i " + i)
+    //console.log("going to next i " + i)
 }
 
-db.count('analystsOpinions')
+
+//new mongodb.Db('test', server, {}).open(function (error, client) {
+//    if (error) throw error;
+//    var collection = new mongodb.Collection(client, 'test_collection');
+//    collection.insert({hello:'world'}, {safe:true},
+//            function (err, objects) {
+//                if (err) console.warn(err.message);
+//                if (err && err.message.indexOf('E11000 ') !== -1) {
+//                    console.log("this _id was already inserted in the database")
+//                }
+//            });
+//    collection.find({hello:'world'}, {limit:10}).toArray(function (err, docs) {
+//        console.dir(docs.length > 0);
+//    });
+//});
